e3c7778ac2e9e176963d68401018cfbfbbe705c1
Karuppasamy99 <karuppasamy.a@hotmail.com>
2024-01-08 12:37:58 +0530

    Reservation form

--- src/components/ReservationForm.jsx
+++ src/components/ReservationForm.jsx
@@ -90,17 +90,17 @@ const ReservationForm = ({ handleRoomReservation, setActive }) => {
                       </label>
                     </div>
                     <div className="relative">
-                    <input
-                  type="date"
-                  name="date"
-                  id="date"
-                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                  value={date}
-                  min={today}
-                  max={endDay}
-                  onChange={(e) => setDate(e.target.value)}
-                  required
-                />
+                      <input
+                        type="date"
+                        name="date"
+                        id="date"
+                        className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                        value={date}
+                        min={today}
+                        max={endDay}
+                        onChange={(e) => setDate(e.target.value)}
+                        required
+                      />
                       <label
                         htmlFor="date"
                         className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
@@ -109,17 +109,16 @@ const ReservationForm = ({ handleRoomReservation, setActive }) => {
                       </label>
                     </div>
                     <div className="relative">
-                    <input
-                  type="time"
-                  name="time"
-                  id="time"
-                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                  value={time}
-                  min={currentTime}
-                 
-                  onChange={(e) => setTime(e.target.value)}
-                  required
-                />
+                      <input
+                        type="time"
+                        name="time"
+                        id="time"
+                        className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                        value={time}
+                        min={currentTime}
+                        onChange={(e) => setTime(e.target.value)}
+                        required
+                      />
                       <label
                         htmlFor="time"
                         className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
@@ -138,7 +137,6 @@ const ReservationForm = ({ handleRoomReservation, setActive }) => {
                   </div>
                 </div>
               </form>
-              
             </div>
           </div>
         </div>
